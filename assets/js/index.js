ef3dcf0283bd77d6893160882bdd2b3e09d8ce7b
Tai An Su <taiansu@gmail.com>
2017-01-20 22:23:54 +0800

    Adjust javascript order

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,13 +9,7 @@
     $(document).ready(function(){
 
         $(".post-content").fitVids();
-        
-        // Calculates Reading Time
-        $('.post-content').readingTime({
-            readingTimeTarget: '.post-reading-time',
-            wordCountTarget: '.post-word-count',
-        });
-        
+
         // Creates Captions from Alt tags
         $(".post-content img").each(function() {
             // Let's put a caption if there is one
@@ -23,7 +17,13 @@
               $(this).wrap('<figure class="image"></figure>')
               .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
         });
-        
+
+        // Calculates Reading Time
+        $('.post-content').readingTime({
+            readingTimeTarget: '.post-reading-time',
+            wordCountTarget: '.post-word-count',
+        });
+
     });
 
 }(jQuery));
