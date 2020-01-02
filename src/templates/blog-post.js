d28548218392df47b3aa4bcde1ea5ed2bd27a81b
Tai An Su <i@taian.su>
2020-01-02 16:33:27 +0800

    Fix mdx render issue

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {MDXRenderer} from "gatsby-plugin-mdx"
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -26,11 +27,10 @@ class BlogPostTemplate extends React.Component {
               {post.frontmatter.date}
             </p>
           </header>
-          <section dangerouslySetInnerHTML={{ __html: post.html }} />
-          <hr className="mb-1" />
-          <footer>
-            <Bio />
-          </footer>
+
+          <MDXRenderer>{post.body}</MDXRenderer>
+
+          <footer/ >
         </article>
 
         <nav>
