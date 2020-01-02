eafde7b528f1c2ad1c0e8a24e51189a4f83bdf78
Tai An Su <i@taian.su>
2020-01-03 01:29:00 +0800

    Rename file using dash

--- src/components/home-header.js
+++ src/components/home-header.js
@@ -6,9 +6,9 @@ import logo from "../../content/assets/logo.png"
 
 const HomeHeader = ({title, description})  => (
   <div className="min-w-sm mt-0 mx-0 mb-3">
-    <img src={banner} className="h-48 md:h-64 lg:h-72 w-full object-cover object-center" />
+    <img src={banner} alt="banner background"  className="h-48 md:h-64 lg:h-72 w-full object-cover object-center" />
     <div className="-mt-12 flex items-center justify-around">
-      <img src={logo} className="h-24 w-24 rounded-full object-cover object-center lg:h-32 lg:w-32" />
+      <img src={logo} alt="logo" className="h-24 w-24 rounded-full object-cover object-center lg:h-32 lg:w-32" />
     </div>
     <h1 className="mx-auto mt-3 text-center text-gray-900 font-sans text-4xl lg:text-5xl">
       <Link className="shadow-none no-underline" to={`/`}>
