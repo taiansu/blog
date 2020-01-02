75fd1b9fe690dc079e6063fb146bb6be65dcf060
Tai An Su <i@taian.su>
2020-01-02 16:34:13 +0800

    Fix asset not found issue

--- src/components/bio.js
+++ src/components/bio.js
@@ -5,7 +5,7 @@ import Image from "gatsby-image"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      avatar: file(absolutePath: { regex: "/author.png/" }) {
         childImageSharp {
           fixed(width: 50, height: 50) {
             ...GatsbyImageSharpFixed
