4bab12304189b325a7b532085bf547965e21dceb
Tai An Su <i@taian.su>
2020-01-05 02:21:45 +0800

    Fix 404 page site info

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,10 +7,17 @@ import SEO from "../components/seo"
 class NotFoundPage extends React.Component {
   render() {
     const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+    const { title: siteTitle, author, description, social } = data.site.siteMetadata
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout
+        location={this.props.location}
+        title={siteTitle}
+        author={author}
+        social={social}
+        description={description}
+      >
+
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
@@ -26,6 +33,13 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+        description
+        author
+        social {
+          twitter
+          github
+          facebook
+        }
       }
     }
   }
