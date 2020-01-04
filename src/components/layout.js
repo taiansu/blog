b532705e5f99f75210c1f1d36ba6fdd5eb913ba3
Tai An Su <i@taian.su>
2020-01-05 03:20:41 +0800

    Improve index footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,46 +11,51 @@ class Layout extends React.Component {
     return (
       <div className="font-sans">
         <div className="min-w-sm mt-0 mx-0 mb-3">
-          <img src={banner} alt="banner background"  className="h-48 md:h-64 lg:h-72 w-full object-cover object-center" />
+          <img src={banner} alt="banner background"  className="h-48 sm:h-64 lg:h-72 w-full object-cover object-center" />
 
           <div className="-mt-12 flex items-center justify-around lg:-mt-16">
             <img src={logo} alt="logo" className="h-24 w-24 rounded-full border-4 border-white shadow-md object-cover object-center lg:h-32 lg:w-32" />
           </div>
 
-          <h2 className="mx-auto mt-2 text-center text-gray-900 tracking-wide text-xl lg:text-2xl">
+          <h2 className="mx-auto mt-2 text-center text-gray-900 tracking-wide text-xl sm:text-2xl lg:text-3xl">
             <Link className="shadow-none no-underline" to={`/`}>
               {title}
             </Link>
           </h2>
 
-          <h1 className="mx-auto text-center text-gray-900 tracking-wide font-normal font-serif text-lg sm:text-2xl md:text-3xl">{description}</h1>
+          <h1 className="mx-auto text-center text-gray-900 tracking-wide font-normal font-serif text-2xl sm:text-3xl lg:text-4xl">{description}</h1>
 
-          <div className="my-3 mx-auto flex items-center justify-center text-gray-700 text-xs sm:text-sm md:text-base">
+          <div className="my-3 mx-auto flex items-center justify-center text-gray-700 text-base sm:text-lg">
             <a href={`https://github.com/${social.github}`}>
               <i className="fab fa-github"></i>
             </a>
-            <span>&nbsp;&nbsp;·&nbsp;&nbsp;</span>
+            <span className="mx-3">·</span>
             <a href={`https://twitter.com/${social.twitter}`}>
               <i className="fab fa-twitter"></i>
             </a>
-            <span>&nbsp;&nbsp;·&nbsp;&nbsp;</span>
+            <span className="mx-3">·</span>
             <a href={`https://www.facebook.com/${social.facebook}`}>
               <i className="fab fa-facebook"></i>
             </a>
-            <span>&nbsp;&nbsp;·&nbsp;&nbsp;</span>
+            <span className="mx-3">·</span>
             <Link to={"/about"}>
             <span>about</span>
             </Link>
           </div>
         </div>
 
-
         <main className="px-8">{children}</main>
 
-        <footer className="my-3 mx-auto text-center text-gray-500">
-          {author} © {new Date().getFullYear()}, build with
-          {` `}
-          <a className="underline" href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer className="mt-8 mb-5 mx-auto text-center text-gray-500">
+          <a href="/rss.xml">Subscribe with{` `}<i className="fas fa-rss ml-1"></i></a>
+          <div>
+            {author} © {new Date().getFullYear()}
+            <span className="mx-3">·</span>
+            <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
+          </div>
+          <div>
+            build with {` `}<a className="underline" href="https://www.gatsbyjs.org">Gatsby</a>
+          </div>
         </footer>
       </div>
     )
