45afeab6e383cc04a360144f0af470def24c8b9e
Tai An Su <i@taian.su>
2020-01-11 05:22:59 +0800

    Add ga tracking-id

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-31272622-1`,
       },
     },
     {
