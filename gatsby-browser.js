4e408f12ff903b58858189cd3e731353e80bca7a
Tai An Su <i@taian.su>
2020-01-10 18:23:04 +0800

    Change code highlight color

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-require("prismjs/themes/prism-okaidia.css")
+require("prismjs/themes/prism.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 
 exports.onRouteUpdate = ({location}) => {
