054dcf32b40a005328f550198406bc810ce6cffa
Tai An Su <i@taian.su>
2020-01-10 18:25:24 +0800

    Adjust index listing format

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,12 @@ import SEO from "../components/seo"
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
-    const { title: siteTitle, author, description, social } = data.site.siteMetadata
+    const {
+      title: siteTitle,
+      author,
+      description,
+      social,
+    } = data.site.siteMetadata
     const posts = data.allMdx.edges
 
     return (
@@ -20,34 +25,35 @@ class BlogIndex extends React.Component {
       >
         <SEO title="All posts" />
         <div className="max-w-md mx-auto mt-6 md:max-w-lg md:mt-6 lg:max-w-xl lg:mt-8">
-          <ul className="ml-2 sm:ml-20 md:ml-24">
-          {posts.map(({ node }, index) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <li key={node.fields.slug} className="mb-2 flex flex-col">
-                { index === 0 ? "" : <hr className="text-gray-900 mb-4 max-w-md"/> }
-                <div className="flex items-baseline mt-2">
-                  <h3 className="inline-block text-gray-700 text-xs uppercase tracking-widest w-1/3 sm:text-sm lg:text-base">
-                    <time>{node.frontmatter.date}</time>
-                  </h3>
-                  <h2 className="inline-block text-base sm:text-lg md:text-xl lg:text-2xl">
-                    <Link to={node.fields.slug}>{title}</Link>
-                  </h2>
-                </div>
-                <div className="hidden flex text-gray-700 mt-1 text-xs sm:inline-block">
-                  <div className="inline-block w-1/3"></div>
-                  <div className="inline-block w-2/3">
-                    { node.frontmatter.tags.length === 0 ?  ""
-                        : (<span className="">
-                            <i className="fa fa-tags text-gray-700 mr-2" />
-                          </span>)
-                    }
-                    <span className="w-2/3">{node.frontmatter.tags.join(", ")}</span>
+          <ul>
+            {posts.map(({ node }, index) => {
+              const title = node.frontmatter.title || node.fields.slug
+              return (
+                <li key={node.fields.slug} className="my-6 w-3/4 mx-auto">
+                  {index === 0 ? (
+                    ""
+                  ) : (
+                    <hr className="text-gray-900 mb-6" />
+                  )}
+                  <div className="flex flex-col justify-start">
+                    <div className="flex items-center text-gray-600">
+                      <time className="text-base">{node.frontmatter.date}</time>
+
+                      { node.frontmatter.tags.length === 0 ?  ""
+                        : (<span className="hidden text-xs sm:inline-block">
+                              <i className="fa fa-tags ml-4 mr-2" />
+                            </span>)
+                      }
+                      <span className="hidden text-xs sm:inline-block">{node.frontmatter.tags.join(", ")}</span>
+
+                    </div>
+                    <h2 className="text-base sm:text-xl sm:mt-1">
+                      <Link to={node.fields.slug}>{title}</Link>
+                    </h2>
                   </div>
-                </div>
-              </li>
-            )
-          })}
+                </li>
+              )
+            })}
           </ul>
         </div>
       </Layout>
@@ -81,7 +87,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "MMM DD, YYYY")
             title
             description
             tags
